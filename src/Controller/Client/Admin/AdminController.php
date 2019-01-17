a0105b9a2a8c51e86ca7854dbb688b8d17858d65
sumitsehgal <theprofessional1992@gmail.com>
2019-01-17 21:07:42 +0530

    My Account Changes

--- src/Controller/Client/Admin/AdminController.php
+++ src/Controller/Client/Admin/AdminController.php
@@ -29,5 +29,25 @@ class AdminController extends AppController
     public function myaccount()
     {
         $this->viewBuilder()->setLayout('admin');
+        $this->loadModel('Users');
+        $user = $this->Users->find('all',[
+            'conditions' => [
+                'username' => $this->Auth->user('username')
+            ]
+        ])->first();
+        
+        if ($this->request->is(['post', 'put'])) {
+            
+            $this->User->patchEntity($user, $this->request->getData());
+            if ($this->user->save($user)) {
+                $this->Flash->success(__('Your user has been updated.'));
+                //return $this->redirect('/client/admin/myaccount');
+            }
+            else
+            {
+                $this->Flash->error(__('Unable to update your user.'));
+            }
+        }
+        $this->set('user', $user);
     }
 }
